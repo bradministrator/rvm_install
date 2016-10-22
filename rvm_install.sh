b287c842fe610b83a64072b260ae9910c00158dd
testy_guy <testy_guy@ps443148.dreamhostps.com>
2016-10-22 16:20:49 -0700

    derped the shell reload within the script - now just comments echo'd after completion

--- rvm_install.sh
+++ rvm_install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-unset GEM_HOME
-source ~/.rvm/scripts/rvm
-echo "source .bash_profile" >> ~/.bashrc
-. ~/.bash_profile
-rvm --version
+echo "running gpg..."
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && 
+echo "pulling rvm..."
+curl -sSL https://get.rvm.io | bash -s stable &&
+echo "unsetting GEM_HOME..."
+unset GEM_HOME &&
+
+echo "updating .bash_profile..."
+echo "source .bash_profile" >> ~/.bashrc &&
+echo "run:  source ~/.rvm/scripts/rvm"
+echo "run:  source ~/.bash_profile"
+echo "check success:  rvm --version"
